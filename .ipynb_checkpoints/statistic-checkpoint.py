f217c68aac3c4525ae123a44f9ffb375892aa2ef
Taro Watanabe <tarowatanabe4@gmail.com>
2023-09-26 22:06:17 +0200

    statistic with plot

--- .ipynb_checkpoints/statistic-checkpoint.py
+++ .ipynb_checkpoints/statistic-checkpoint.py
@@ -5,6 +5,8 @@ import os
 from datetime import datetime as dt
 import calendar
 import warnings
+import matplotlib.pyplot as plt
+import fit
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
@@ -29,6 +31,14 @@ def sorterkey(line):
     time = line[21:26].replace('-', '')
     
     return (int(year), int(month), int(day), int(time))
+
+def ticker(tick, temp):
+    minTick = np.floor(np.min(temp) / tick) * tick
+    maxTick = np.ceil(np.max(temp) / tick) * tick
+    major = np.arange(minTick, maxTick, tick)
+    minor = np.arange(minTick, maxTick, tick/2)
+    
+    return major, minor
 
 def statistic():
     path = './txt/'
@@ -100,8 +110,9 @@ def statistic():
     print(f'card:  {np.mean(card):7.3f} +/- {np.std(card):6.3f}')
 
     print('')
+    print('')
 
-    print('top three')
+    print('top three: total')
     line = ' '*6 + 'total' + ' '*6 + 'ratio' + ' '*8 + 'timestamp'
     print(line)
     line = '-'*3 + '+' + '-'*9 + '+' + '-'*10 + '+' + '-'*18
@@ -113,7 +124,23 @@ def statistic():
         print(f'{i+1}":  {float(top[i][0]):6.2f}€   {float(top[i][1]):5.3f}€/h   {top[i][2]:10} {wkday} {top[i][3]}')
         print(f'{" "*5}holiday -> {top[i][4].capitalize()}')
         
+    print('')
+    print('')
+    
+    print('top three: ratio')
+    line = ' '*6 + 'ratio' + ' '*6 + 'total' + ' '*8 + 'timestamp'
+    print(line)
+    line = '-'*3 + '+' + '-'*9 + '+' + '-'*10 + '+' + '-'*18
+    print(line)
 
+    top = sorted(zip(ratio, total, date, time, holiday), reverse=True)[:3]
+    for i in range(len(top)):
+        wkday = dt.strptime(top[i][2], '%d.%m.%Y').strftime('%a')
+        print(f'{i+1}": {float(top[i][0]):5.2f}€/h   {float(top[i][1]):6.3f}€   {top[i][2]:10} {wkday} {top[i][3]}')
+        print(f'{" "*5}holiday -> {top[i][4].capitalize()}')
+        
+
+    print('')
     print('')
 
     total = np.array(total)
@@ -162,3 +189,152 @@ def statistic():
 
         print('')
         
+    print('')
+    print('rendering plots')
+    
+    Ptime = [[] for _ in range(4)]
+    Ptotal = [[] for _ in range(7)]
+    Pbar = [[] for _ in range(7)]
+    Pcard = [[] for _ in range(7)]
+    Pall = [[] for _ in range(14)]
+
+    Rtime = [[] for _ in range(4)]
+    Rtotal = [[] for _ in range(7)]
+    Rbar = [[] for _ in range(7)]
+    Rcard = [[] for _ in range(7)]
+    Rall = [[] for _ in range(14)]
+
+
+    AM = [i == 'AM' for i in time]
+
+    ferien = [i != 'False' for i in holiday]
+    for i in range(len(weekday)):
+            if 3 < weekday[i] < 7:
+                ferien[i] = True
+
+    for i in range(len(total)):
+        if AM[i]:
+            Ptime[0].append(total[i])
+            Rtime[0].append(ratio[i])
+        else:
+            Ptime[1].append(total[i])
+            Rtime[1].append(ratio[i])
+
+        if ferien[i]:
+            Ptime[2].append(total[i])
+            Rtime[2].append(ratio[i])
+        else:
+            Ptime[3].append(total[i])
+            Rtime[3].append(ratio[i])
+
+        total[i] = float(total[i])
+
+        Ptotal[weekday[i]].append(total[i])
+        Rtotal[weekday[i]].append(ratio[i])
+
+    for i in range(len(bar)):
+        if bar[i] != 'None':
+            Pbar[weekday[i]].append(bar[i])
+        if card[i] != 'None':
+            Pcard[weekday[i]].append(card[i])
+
+    for i in range(1, 8):
+        Pall[i *2 -2] = Pbar[i -1]
+        Pall[i *2 -1] = Pcard[i -1]
+
+    c = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink']
+
+    fit.saveplot(fig_width=6.5)
+
+    fig, ax = plt.subplots()
+    for i in range(4):
+        ax.plot(np.ones(len(Ptime[i])) *i +1, Ptime[i], ms=4, marker='o', mew=0.5, ls="none", color=c[i])
+    ax.boxplot(Ptime)
+    temp = [item for row in Ptime for item in row]
+
+    major, minor = ticker(20, temp)
+    ax.set_yticks(major)
+    ax.set_yticks(minor, minor = True)
+    plt.xticks(np.arange(5), ['', 'AM', 'PM', 'holiday', 'normal'])
+
+    plt.ylabel('€')
+    plt.grid(axis = 'y', which = 'major', alpha = 0.7)
+    plt.grid(axis = 'y', which = 'minor', alpha = 0.3)
+    plt.show()
+
+
+    fig, ax = plt.subplots()
+    for i in range(4):
+        ax.plot(np.ones(len(Rtime[i])) *i +1, Rtime[i], ms=4, marker='o', mew=0.5, ls="none", color=c[i])
+    ax.boxplot(Rtime)
+    temp = [item for row in Rtime for item in row]
+
+    major, minor = ticker(1, temp)
+    ax.set_yticks(major)
+    ax.set_yticks(minor, minor = True)
+    plt.xticks(np.arange(5), ['', 'AM', 'PM', 'holiday', 'normal'])
+
+    plt.ylabel('€/h')
+    plt.grid(axis = 'y', which = 'major', alpha = 0.7)
+    plt.grid(axis = 'y', which = 'minor', alpha = 0.3)
+    plt.show()
+
+
+    fig, ax = plt.subplots()
+    for i in range(7):
+        ax.plot(np.ones(len(Ptotal[i])) *i +1, Ptotal[i], ms=4, marker='o', mew=0.5, ls="none", color=c[i])
+    ax.boxplot(Ptotal)
+    temp = [item for row in Ptotal for item in row]
+
+    major, minor = ticker(20, temp)
+    ax.set_yticks(major)
+    ax.set_yticks(minor, minor = True)
+    plt.xticks(np.arange(8), ['', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
+
+    plt.title("total tip")
+    plt.xlabel('weekday')
+    plt.ylabel('€')
+    plt.grid(axis = 'y', which = 'major', alpha = 0.7)
+    plt.grid(axis = 'y', which = 'minor', alpha = 0.3)
+    plt.show()
+
+
+    fig, ax = plt.subplots()
+    for i in range(7):
+        ax.plot(np.ones(len(Rtotal[i])) *i +1, Rtotal[i], ms=4, marker='o', mew=0.5, ls="none", color=c[i])
+    ax.boxplot(Rtotal)
+    temp = [item for row in Rtotal for item in row]
+
+    major, minor = ticker(1, temp)
+    ax.set_yticks(major)
+    ax.set_yticks(minor, minor = True)
+    plt.xticks(np.arange(8), ['', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
+
+    plt.title("total tip")
+    plt.xlabel('weekday')
+    plt.ylabel('€/h')
+    plt.grid(axis = 'y', which = 'major', alpha = 0.7)
+    plt.grid(axis = 'y', which = 'minor', alpha = 0.3)
+    plt.show()
+
+
+    fig, ax = plt.subplots()
+    for i in range(14):
+        ax.plot(np.ones(len(Pall[i])) *i +1, Pall[i], ms=4, marker='o', mew=0.5, ls="none", color=c[(i)//2])
+    ax.boxplot(Pall)
+    temp = [item for row in Pall for item in row]
+
+    major, minor = ticker(20, temp)
+    ax.set_yticks(major)
+    ax.set_yticks(minor, minor = True)
+    plt.xticks(np.arange(15), ['', 'Mon','' , 'Tue', '', 'Wed', '', 'Thu', '', 'Fri', '', 'Sat', '', 'Sun', ''])
+
+    plt.title("bar, card")
+    plt.xlabel('weekday')
+    plt.ylabel('€')
+    plt.grid(axis = 'y', which = 'major', alpha = 0.7)
+    plt.grid(axis = 'y', which = 'minor', alpha = 0.3)
+    plt.show()
+    
+    
+    print('render plots done.')
